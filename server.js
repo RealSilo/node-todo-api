336c32e83307423ea5f4186f057384badc49cc32
RealSilo <szilard.magyar@gmail.com>
2016-08-13 15:20:35 +0200

    delete

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send("todo api root");
 });
 
-app.get('/todos', function(req, res) {
+app.get('/todos', function (req, res) {
   res.json(todos);
 });
 
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   todos.map((todo) => {
     if (todo.id === todoId) {
@@ -27,7 +27,7 @@ app.get('/todos/:id', function(req, res) {
   res.status(404).send();
 });
 
-app.post('/todos', function(req, res) {
+app.post('/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
@@ -39,6 +39,18 @@ app.post('/todos', function(req, res) {
   todos.push(body);
 
   res.json(body);
+});
+
+app.delete('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    response.status(404).json({"error": "no todo found with this id"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
 });
 
 app.listen(PORT, function() {
